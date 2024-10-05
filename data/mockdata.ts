d2a4ad1f3e189ec1001e13b7a2d6ce60c894a085
mewmaze <mewmaze@naver.com>
2024-10-05 16:06:25 +0900

    getMockData 함수 추가

--- data/mockdata.ts
+++ data/mockdata.ts
@@ -715,3 +715,20 @@ interface MockData {
       boughtDate: "Sun May 18 2014 05:12:12 GMT+0900 (한국 표준시)",
     },
   ];
+
+  const PER_PAGE = 10;
+
+  // 페이지는 1부터 시작함
+  export const getMockData = (pageNum: number) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const datas: MockData[] = MOCK_DATA.slice(
+          PER_PAGE * pageNum,
+          PER_PAGE * (pageNum + 1)
+        );
+        const isEnd = PER_PAGE * (pageNum + 1) >= MOCK_DATA.length;
+  
+        resolve({ datas, isEnd });
+      }, 1500);
+    });
+  };
